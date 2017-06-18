6d8186560ff70350f387c555b6308964fb8e87b6
palyla <palyla@ya.ru>
2017-06-19 00:27:45 +0300

    Initial cvetovichkof.ru parsing

--- sites/parsers/__init__.py
+++ sites/parsers/__init__.py
@@ -1,5 +1,5 @@
 from sites.parsers.barselona import Barselona
-from sites.parsers.cvetovichkofru import Cvetovichkofru
+#from sites.parsers.cvetovichkofru import Cvetovichkofru
 from sites.parsers.cvetovikcom import Cvetovikcom
 from sites.parsers.cvetopttorgspbru import Cvetopttorgspbru
 from sites.parsers.orangespbru import Orangespbru
--- sites/parsers/cvetovichkofru.py
+++ sites/parsers/cvetovichkofru.py
@@ -1,3 +1,13 @@
+from urllib import request
+
+import io
+from bs4 import BeautifulSoup
+import pdfminer
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+
 from sites import SitesFactory
 
 
@@ -6,4 +16,33 @@ class Cvetovichkofru(SitesFactory):
 
     @classmethod
     def get_raw_data(cls):
+        pdf = PDFParser(cls.get_pdf())
+        doc = PDFDocument()
+        pdf.set_document(doc)
+        doc.set_parser(pdf)
+        doc.initialize('')
+
+        rsrcmgr = PDFResourceManager()
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        for page in doc.get_pages():
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for lt_obj in layout:
+                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                    print(lt_obj.get_text())
+
         return None
+
+    @classmethod
+    def get_pdf(cls):
+        return request.urlopen("https://cvetovichkof.ru/images/prays_opt.pdf")
+
+    @classmethod
+    def get_page(cls, url):
+        print("GET  {}".format(url))
+        page = request.urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+        return soup
