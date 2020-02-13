50d0d1316f9b81403608bb9f64f0020329d60dba
Alexey Sedlyarsky <palyla@ya.ru>
2020-02-13 12:36:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from viewers.console import ConsoleViewer
 from controllers.flowers_controller import FlowersController
 from utils import config
 
+
 conf = config.Configuration()
 config.Configuration = conf
 
